e1436dfb1c626ca093f1ca9b3c158694d2933235
Saurabh Mahendra Pakhale <smpakhale36@gmail.com>
2024-02-24 11:55:34 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,35 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
+import{ BrowserRouter } from 'react-router-dom';
+// import { Link } from "react-router-dom";
+
 
 export default function Navbar(props) {
   return (
+    <BrowserRouter>
     <nav
-      className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+    className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
     >
-      <a className="navbar-brand" href="/">
+      <a className="navbar-brand" href="#">
         {props.title}
       </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="/">
+            <a className="nav-link" href="#">
               Home <span className="sr-only">(current)</span>
             </a>
           </li>
           <li className="nav-item active">
-            <a className="nav-link" href="/">
+            <a className="nav-link" href="#">
               {props.about} <span className="sr-only">(current)</span>
             </a>
           </li>
@@ -50,13 +46,14 @@ export default function Navbar(props) {
             type="checkbox"
             role="switch"
             id="flexSwitchCheckDefault"
-          />
+            />
           <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
-            Enable dark Mode
+            Enable Dark Mode
           </label>
         </div>
       </div>
     </nav>
+    </BrowserRouter>
   );
 }
 
@@ -67,5 +64,5 @@ Navbar.propTypes = {
 
 Navbar.defaultProps = {
   title: "SetTitleHere",
-  about: "AboutTextHere",
+  about: "AboutTextUtils",
 };
